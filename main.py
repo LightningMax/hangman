c0e10fc23b53870387de7d402e639d1226c9e632
Max <maxreis507@gmail.com>
2024-01-05 13:44:33 +0100

    FIX: Now it keeps the result

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
+def create_map(word):
+    map = ''
+    for i in word:
+        map += '_'
+    
+    return list(map)
+
+def convert_to_list(word):
+    pass
+    #map_word = create_map(word)
+    word_to_convert = []
+    for i in word:
+        if i == player_input:
+            word_to_convert.append(i)
+        else:
+            word_to_convert.append('_ ')
+
+    return ', '.join(word_to_convert)
+
+def convert_to_string(word):
+    pass
+
 words = ['reality', 'fundamental']
 hangman_word = ''
+word_to_convert = create_map(words[0])
 
-while hangman_word != words[0]:
+
+while True:
+
+    print(word_to_convert)
     player_input = input("Guess a letter\n~> ")
-    
-    for i in words[0]:
-        if player_input == i:
-            hangman_word += player_input
-        else:
-            hangman_word += "_ "
+    for i, char in enumerate(words[0]):
+        if char == player_input:
+            word_to_convert[i] = char
 
-    print(hangman_word)
+#hangman_word = convert_to_list(words[0])
