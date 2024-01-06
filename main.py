24866c09dd87e1d2642eb368747f7d05307b56da
Max <maxreis507@gmail.com>
2024-01-06 18:43:01 +0100

    added ascii arts

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
-from wordlist import *
+from word_list import *
+from ASCII_arts import *
 
 def create_map(word):
     map = ''
@@ -9,17 +10,19 @@ def create_map(word):
 
 random_number = random.randint(0 , len(word_list))
 words = word_list[random_number]
-hangman_word = ''
 word_to_convert = create_map(words)
 player_lives = 6
+hangman_stage = 0
 
 while player_lives > 0:
-    print(f"lives: {player_lives}")
+    print(HANGMANPICS[hangman_stage])
     print(' '.join(word_to_convert))
     player_input = input("Guess a letter\n~> ").lower()
+
     if player_input not in words:
         print("This letter is not in the word")
         player_lives -= 1
+        hangman_stage +=1
     elif player_input in word_to_convert:
         print("You already typed this letter")
     else:
