f26285cae9f99155fd8a807a35ebac784b67548e
Max <maxreis507@gmail.com>
2024-01-12 11:32:54 +0100

    Refactor: rename variables, simplify logic etc.. - renamed ASCII_arts file to ascii_arts - word_list file refactored - renamed main variables - added commentaries in the main file

--- main.py
+++ main.py
@@ -2,42 +2,46 @@ import os
 import random
 import time
 from word_list import *
-from ASCII_arts import *
+from ascii_arts import *
 
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def mapping_underscore(word):
-    map = ''
-    for i in word:
-        map += '_' 
-    return list(map)
-
 random_number = random.randint(0 , len(word_list))
+
 word_to_guess = word_list[random_number]
-player_guess = mapping_underscore(word_to_guess)
+
 player_lives = 6
+
 hangman_stage = 0
+
 typed_words = []
+
+list_display = []
+for _ in word_to_guess:
+    list_display += '_'
+
 
 while True:
     clear_screen()
-    print(HANGMANPICS[hangman_stage])
-    formated_player_guess = ''.join(player_guess)
 
-    # Win Lose Conditions
-    if formated_player_guess == word_to_guess:
+    print(HANGMANPICS[hangman_stage])
+
+    # Win Condition
+    display = ''.join(list_display)
+    if display == word_to_guess:
         print("You Win!")
         break
     
+    # Lose Condition
     if player_lives < 1:
         print("You Lose!")
         print(f"The word was {word_to_guess}")
         break
-
-    print(' '.join(formated_player_guess))
-    player_input = input("Guess a letter\n~> ").lower()
     
+    print(' '.join(display))
+    player_input = input("Guess a letter\n~> ").lower()
+     
     if player_input in typed_words:
         print("You already typed this letter")
         time.sleep(2)
@@ -47,8 +51,8 @@ while True:
         player_lives -= 1
         hangman_stage +=1
     
-    for i, char in enumerate(word_to_guess):
-        if char == player_input:
-            player_guess[i] = char
+    for position, word in enumerate(word_to_guess):
+        if word == player_input:
+            list_display[position] = word
     
     typed_words.append(player_input)
